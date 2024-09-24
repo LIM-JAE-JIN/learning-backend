cf77945f540fb7bd002add76a08d4f98e74c3864
JAEJIN <wowls9416@naver.com>
2024-09-24 11:34:53 +0900

    NestJS - 상품 관련entity 구현 및 sql query 작성

---
+++ class/section10/10-01-mysql-relation/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,46 @@
+import { ProductCategory } from 'src/apis/productsCategorys/entities/productsCategory.entity';
+import { ProductSaleslocation } from 'src/apis/productsSaleslocations/entities/productsSaleslocation.entity';
+import { ProductTag } from 'src/apis/productsTags/entities/productsTag.entity';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column({ default: false })
+  isSoldout: boolean;
+
+  @JoinColumn()
+  @OneToOne(() => ProductSaleslocation)
+  productSaleslocation: ProductSaleslocation;
+
+  @ManyToOne(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @JoinTable()
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products)
+  productTags: ProductTag[];
+}
