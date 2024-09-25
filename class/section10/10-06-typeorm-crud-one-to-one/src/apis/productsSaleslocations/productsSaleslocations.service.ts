8d0fa6ed78c501367933912629bde7f64309bfcb
JAEJIN <wowls9416@naver.com>
2024-09-25 12:06:26 +0900

    1:1 관계등록 API 상품생성 시 상품테이블, 상품주소테이블 데이터 등록 및 조회 작업

---
+++ class/section10/10-06-typeorm-crud-one-to-one/src/apis/productsSaleslocations/productsSaleslocations.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ProductSaleslocation } from './entities/productsSaleslocation.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProductsSaleslocationsService {
+  constructor(
+    @InjectRepository(ProductSaleslocation)
+    private readonly productSaleslocationRepository: Repository<ProductSaleslocation>,
+  ) {}
+
+  create({ productSaleslocation }) {
+    return this.productSaleslocationRepository.save({
+      ...productSaleslocation,
+    });
+  }
+}
