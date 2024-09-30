b2f714688d3f65531b5bfde41fbc1f64af7bbbdb
JAEJIN <wowls9416@naver.com>
2024-09-30 11:05:27 +0900

    포트원 결제 관련 API 트렌잭션 isolation 접근 설정 및 데드락 확인

---
+++ class/section12/12-04-isolation/backend/src/apis/pointsTransactions/pointsTransactions.service.ts
@@ -0,0 +1,72 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import {
+  POINT_TRANSACTION_STATUS_ENUM,
+  PointTransaction,
+} from './entities/pointTransaction.entity';
+import { IPointsTransactionsServiceCreate } from './interfaces/points-transactions-service.interface';
+import { User } from '../users/entities/user.entity';
+
+@Injectable()
+export class PointsTransactionsService {
+  constructor(
+    @InjectRepository(PointTransaction)
+    private readonly pointsTransactionsRepository: Repository<PointTransaction>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  async create({
+    impUid,
+    amount,
+    user: _user,
+  }: IPointsTransactionsServiceCreate): Promise<PointTransaction | void> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      // 1, PointTransaction 테이블에 거래기록 1줄 생성
+      const pointTransaction = this.pointsTransactionsRepository.create({
+        impUid,
+        amount,
+        user: _user,
+        status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+      });
+      // await this.pointsTransactionsRepository.save(pointTransaction);
+      await queryRunner.manager.save(pointTransaction);
+
+      // 2. 유저의 돈 찾아오기
+      // const user = await this.userRepository.findOne({
+      //   where: { id: _user.id },
+      // });
+      const user = await queryRunner.manager.findOne(User, {
+        where: { id: _user.id },
+      });
+
+      // 3. 유저의 돈 업데이트
+      // await this.userRepository.update(
+      //   { id: _user.id },
+      //   { point: user.point + amount },
+      // );
+      await queryRunner.manager.update(
+        User,
+        { id: _user.id },
+        { point: user.point + amount },
+      );
+
+      await queryRunner.commitTransaction();
+
+      // 4. 최종결과 브라우저에 돌려주기
+      return pointTransaction;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      console.log(error);
+    } finally {
+      // release가 없으면, commit 또는 rollback 후 connection이 끊기지 않아 과부화 (에러 시 자동 끊김)
+      await queryRunner.release();
+    }
+  }
+}
